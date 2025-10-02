f98377290ab77823b864266be6fc2dc6cd9222e4
Alejandro <alejandroherranz1603@gmail.com>
2025-10-01 22:21:02 -0600

    Implemented Chests, doors, keys and loot.

--- My2DGame/src/object/OBJ_Key.java
+++ My2DGame/src/object/OBJ_Key.java
@@ -4,13 +4,34 @@ import entity.Entity;
 import main.GamePanel;
 
 public class OBJ_Key extends Entity{
+	
+	GamePanel gp;
 
 	public OBJ_Key(GamePanel gp) {
 		super(gp);
+		this.gp = gp;
 		
+		type = type_consumable;
 		name = "Key";
 		down1 = setup("/objects/key", gp.tileSize, gp.tileSize);
 		description = "[" + name + "]\nThis opens something...";
 		price = 100;
+	}
+	public boolean use(Entity entity) {
+		
+		gp.gameState = gp.dialogueState;
+		
+		int objIndex = getDetected(entity, gp.obj, "Door");
+		
+		if(objIndex != 999) {
+			gp.ui.currentDialogue = "You used the " + name + " and opened the door.";
+			gp.playSE(3);
+			gp.obj[gp.currentMap][objIndex] = null;
+			return true;
+		}
+		else {
+			gp.ui.currentDialogue = "There is nothing to open around here...";
+			return false;
+		}
 	}
 }
--- My2DGame/src/object/OBJ_Potion_Red.java
+++ My2DGame/src/object/OBJ_Potion_Red.java
@@ -22,13 +22,14 @@ public class OBJ_Potion_Red extends Entity{
 		description = "[" + name + "]\nHeals your life by " + value + ".";
 		price = 25;
 	}
-	public void use(Entity entity) {
+	public boolean use(Entity entity) {
 		
 		gp.gameState = gp.dialogueState;
 		gp.ui.currentDialogue = "You drank the " + name + "!\n"
 				+ "Your life has been recovered by " + value + ".";
 		entity.life += value;
 		gp.playSE(2);
+		return true;
 	}
 
 }
